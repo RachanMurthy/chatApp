127cb33ded23679bd62cf1a47f79eb6c36d2a8ec
Rachan Murthy <rachan3thi@gmail.com>
2024-06-28 20:57:21 +0530

    FrontendBackend Connection Using Axios

--- Frontend/src/routes/loginPage/loginPage.jsx
+++ Frontend/src/routes/loginPage/loginPage.jsx
@@ -1,10 +1,38 @@
 import "./loginPage.scss";
 import { Link } from "react-router-dom";
+import apiRequest from "../../../lib/apiRequest";
+import { useState } from "react";
 
 function LoginPage(){
+
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        setError(null);
+        e.preventDefault();
+        const formdata = new FormData(e.target);
+
+
+        const username = formdata.get("username");
+        const password = formdata.get("password");
+        console.log(password, username);
+
+        try{
+            const response = await apiRequest.post("/auth/login", {
+                username,
+                password
+            });
+        
+        }catch(err){
+            console.log(err.response.data.message);
+            setError(err.response.data.message)
+        }
+    }
+
     return (
         <div className="loginPage">
-            <form>
+            <form onSubmit={handleSubmit}>
+                {error && <p>{error}</p>}
                 <h2>Login</h2>
 
                 <label>Username</label>
--- Frontend/src/routes/registerPage/registerPage.jsx
+++ Frontend/src/routes/registerPage/registerPage.jsx
@@ -1,10 +1,40 @@
 import "./registerPage.scss";
+import apiRequest from "../../../lib/apiRequest";
+import { useState } from "react";
 
 function RegisterPage(){
+
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        setError(null);
+        e.preventDefault();
+        const formdata = new FormData(e.target);
+
+        const email = formdata.get("email");
+        const username = formdata.get("username");
+        const password = formdata.get("password");
+        console.log(email, password, username);
+
+        try{
+            const response = await apiRequest.post("/auth/register", {
+                username,
+                password,
+                email
+            });
+    
+            console.log(response.data);
+        }catch(err){
+            console.log(err);
+            setError(err.response.data.message)
+        }
+
+    
+    }
     return (
         <div className="registerPage">
             
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Register</h2>
                 <label>Email</label>
                 <input name="email" required/>
